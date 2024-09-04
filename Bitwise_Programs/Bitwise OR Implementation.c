ffc345a78f1b98b44d32d2e7d950db6a45054434
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-09-04 08:03:30 +0530

    Bitwise Programs

---
+++ Bitwise_Programs/Bitwise OR Implementation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1,No2,Res=0;
+    printf("\n Enter Two Numbers : \n");
+    scanf("%d%d",&No1,&No2);
+    printf("************===========*************");
+    Res=No1 | No2;
+    printf("\n Result =>%d | %d = %d.",No1,No2,Res);
+    printf("\n************===========*************");
+    getch();
+    return 0;
+}
