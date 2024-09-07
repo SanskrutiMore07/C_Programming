03d1734b28a6672b6da61de27cfb7bbe0ee3b1dd
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-09-07 11:41:04 +0530

    Basic Program

---
+++ Basic_Programs/Addition_Of_Two_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1,No2,Res;
+
+    printf("\n Enter two numbers :");
+    scanf("%d%d",&No1,&No2);
+
+    Res=No1+No2;
+
+    printf("\n Addition Of Given Numbers :%d",Res);
+
+    getch();
+    return 0;
+}
