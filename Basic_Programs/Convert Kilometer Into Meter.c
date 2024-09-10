ad9a9e7af8a69592e3b0410a0aad595dd07117be
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-09-10 22:26:30 +0530

    Basic Program

---
+++ Basic_Programs/Convert Kilometer Into Meter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    float km,m;
+
+    printf("\n Enter value in km:");
+    scanf("%d",&km);
+    m= km*100;
+    printf("\n m=%f",m);
+
+    getch();
+
+    return 0;
+}
