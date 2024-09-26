cf0905df01bb3e30a2046f235d62259bd5efd8f2
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-09-26 18:51:23 +0530

    Table Program

---
+++ Basic_Programs/Table programs/Program to print table of 20 to 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>s
+int main()
+{
+    int R=0,C=0;
+    printf("\n=======================\n\n");
+    for(R=1;R<=10;R++)
+    {
+        for(C=20;C>=2;C--)
+        {
+            printf("%-4d",R*C);
+        }
+        printf("\n\n");
+
+    }
+    printf("\n==========================\n");
+    getch();
+    return 0;
+}
