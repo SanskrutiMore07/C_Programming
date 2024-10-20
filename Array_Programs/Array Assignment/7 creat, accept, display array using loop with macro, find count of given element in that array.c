43ef54efac144c9bf924cfc23fae537f74a73ae1
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-20 21:27:50 +0530

    Assignment 7

---
+++ Array_Programs/Array Assignment/7 creat, accept, display array using loop with macro, find count of given element in that array.c
@@ -0,0 +1,45 @@
+//Create, Accept, Display Array Using Loop - with macro ,Find Count of Given Element in that Array//
+#include<stdio.h>
+#include<conio.h>
+#define size 5
+
+int main()
+{
+    int Arr[size] = {} , i = 0 , Src_Element = 0 ,Cnt  = 0;
+
+    printf("\n Accept Array ");
+
+    for( i = 0 ; i < size ; i++)
+    {
+        printf("\n\t Enter %d Element = ", i );
+        scanf("%d",&Arr[i]);
+    }
+
+    printf("\n Display The Array Element ");
+
+    for( i = 0 ; i < size ; i++)
+    {
+        printf("\n  Element Of %d Array =%d ", i , Arr[i]);
+    }
+
+    printf("\n Press any key to Continue !!!");
+    getch();
+
+    printf("\n Search Element For Count Of Its Element =");
+
+    printf("\n Search The Element Of That Array = ");
+    scanf("%d",&Src_Element);
+
+    for( i = 0 ;i < size ; i++)
+    {
+        if(Arr[i]==Src_Element)
+        {
+            Cnt++;
+        }
+    }
+
+    printf("\n The Count Of %d Element In That Array = %d ",Src_Element , Cnt);
+
+    getch();
+    return 0;
+}
