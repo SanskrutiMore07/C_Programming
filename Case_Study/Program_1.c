1d4c516bfd24d34d9a2352df700f1b6cf109b7c2
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-01-06 23:04:43 +0530

    Case Study

---
+++ Case_Study/Program_1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct stud
+{
+    int Roll_No;
+    char Name[40];
+    int phy;
+    int chem;
+    int maths;
+    float Total;
+    float Per;
+};
+int main()
+{
+    struct stud std;
+    std.Roll_No =01;
+    strcpy(std.Name,"Sayali More");
+    std.phy = 90;
+    std.chem = 60;
+    std.maths = 70;
+
+    std.Total = std.phy + std.maths + std.chem;
+    std.Per = (std.Total/300)*100;
+
+    printf("\n Roll No of student : %d",std.Roll_No);
+    printf("\n Name Of Student :%s",std.Name);
+    printf("\n Total Marks :%0.0f",std.Total);
+    printf("\n Percentage :%0.3f",std.Per);
+
+    printf("\n Thanks!!!!");
+    getch();
+    return 0;
+}
