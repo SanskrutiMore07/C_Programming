d0c97f373df20d8cd59b279c71c130f464e66881
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-02-02 20:00:32 +0530

    Storage_Class

---
+++ Storage _Class/02 Static Basic With Static Keyword.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Book_Read();
+
+int main()
+{
+
+    int No = 0;
+
+    No = Book_Read();
+    printf("\n Pages Read By Day 1 = %d \n",No);
+
+    No = Book_Read();
+    printf("\n Pages Read By Day 2 = %d \n",No);
+
+    No = Book_Read();
+    printf("\n Pages Read By Day 3 = %d \n",No);
+
+    getch();
+    return 0;
+
+}
+int Book_Read()
+{
+   static int Res = 10;
+    int PCnt = 0;
+
+    printf("\n How May Pages Read Today : ");
+    scanf("%d",&PCnt);
+
+    Res = Res+PCnt;
+
+    return Res;
+}
+
+
