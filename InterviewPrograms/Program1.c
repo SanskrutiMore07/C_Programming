515a7e4cc1881d8e8c0ba5f77e8798fcfcb38b28
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-02-01 23:19:09 +0530

    Program

---
+++ InterviewPrograms/Program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0 , Sum = 0;
+
+    printf("Enter Two Numbers : ");
+    scanf("%d%d",&No1,&No2);
+
+    Sum = No1+ No2;
+
+    printf("\n Sum Of Given Numbers %d And %d Is :%d",No1,No2,Sum );
+    return 0;
+
+}
